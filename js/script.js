0139b01a29c2882eb70bfd35a141941a9026b6ae
Raul Reyes malqui <rerm1992@gmail.com>
2016-04-14 20:28:29 +1200

    Changes made to css, js and html. Responsive side bar and Home Page. Buttons Linking to sections of page

--- js/script.js
+++ js/script.js
@@ -1,81 +1,64 @@
+/*Push the body and nav over by 285px to reveal sidebar when menu element is clicked*/
 var main = function() {
-  /* Push the body and the nav over by 285px */
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
-
-    $('body').animate({
-      left: "285px"
-    }, 200);
-  });
-
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
-
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
+    $(".icon-menu").click(function() {
+        $("#sidebar").animate({
+            left: "0px"
+        }, 200), $("body").animate({
+            left: "285px"
+        }, 200)
+    }), 
+/*...and then 'hide' sidebar when close element is clicked */ 
+    $(".icon-close").click(function() {
+        $("#sidebar").animate({
+            left: "-285px"
+        }, 200), $("body").animate({
+            left: "0px"
+        }, 200)
+    }), 
+/*...also hide side bar when link is clicked to reveal 100% of page */ 
+    $("#sidebar li").click(function() {
+        $("#sidebar").animate({
+            left: "-285px"
+        }, 200), $("body").animate({
+            left: "0px"
+        }, 200)
+    })
 };
-
 
 $(document).ready(main);
 
-
-jQuery(function($) {
-
-  var $sections = $('.section'),
-    $animContainer = $('html, body'),
-    $document = $(document),
-    numSections = $sections.length,
-    currSection = 0,
-    isAnimating = false;
-
-  // Animate to a specific index.
-  var gotoSection = function(index) {
-    isAnimating = true;
-    $animContainer.animate({
-      scrollTop: $sections.eq(index).offset().top
-    }, 750, function() {
-      isAnimating = false;
-    });
-  };
-
-  // Advance to next or previous section.
-  var handleAction = function(direction) {
-    if (isAnimating) {
-      return false;
-    }
-
-    if (direction === 'prev' && currSection > 0) {
-      currSection--;
-    } else if (direction === 'next' && currSection < numSections - 1) {
-      currSection++;
-    } else {
-      return false;
-    } // This shouldn't happen.
-
-    gotoSection(currSection);
-  };
-
-  // Handle clicks.
-  $document.on('click', '.action', function() {
-    handleAction($(this).data('direction'));
-  });
-
-  // Handle keyboard input.
-  $document.keyup(function(e) {
-    if (e.keyCode === 38) {
-      handleAction('prev');
-    } // Up arrow.
-    if (e.keyCode === 40) {
-      handleAction('next');
-    } // Down arrow.
-  });
-
+jQuery(function(n) {
+    var e = n("#section"),
+        t = n("html, body"),
+        o = n(document),
+        i = e.length,
+        c = 0,
+        f = !1,
+        u = function(n) {
+            f = !0, t.animate({
+                scrollTop: e.eq(n).offset().top
+            }, 750, function() {
+                f = !1
+            })
+        },
+        r = function(n) {
+            if (f) return !1;
+            if ("prev" === n && c > 0) c--;
+            else {
+                if (!("next" === n && i - 1 > c)) return !1;
+                c++
+            }
+            u(c)
+        };
+    o.on("click", ".action", function() {
+        r(n(this).data("direction"))
+    }), o.keyup(function(n) {
+        38 === n.keyCode && r("prev"), 40 === n.keyCode && r("next")
+    })
 });
 
+$(".button2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section browse").offset().top
+    }, 3000);
+});
